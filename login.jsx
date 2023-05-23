841d1ada1dde403c63b3ad9c888d4a360f369e4a
Irteza alii <76775519+Irtezalii@users.noreply.github.com>
2023-05-23 12:23:52 +0500

    Add files via upload

---
+++ login.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" autoComplete="off" />
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" autoComplete="off" />
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    )
+}
